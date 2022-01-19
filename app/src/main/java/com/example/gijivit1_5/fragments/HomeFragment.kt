7a03574acfd4946af1200e9749359b9edec5e59e
Lukito24 <luka.orjonikidze.1@btu.edu.ge>
2022-01-20 03:36:51 +0400

    gijivit1-5

--- app/src/main/java/com/example/gijivit1_5/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/gijivit1_5/fragments/HomeFragment.kt
@@ -59,30 +59,184 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         )
         dayList.add(
             Day("Day 5", "Push Day",
+                R.drawable.diamond_pushup, "Diamond Push-Ups", "2 Sets of 8 Repetitions",
+                R.drawable.pike_pushup, "Pyke Push-Ups", "2 Sets of 8 Repetitions",
+                R.drawable.wide_pushup, "Wide Push-Ups", "2 Sets of 8 Repetitions"
+            )
+        )
+        dayList.add(
+            Day("Day 6", "Pull Day",
+                R.drawable.pullup, "Pull-Ups", "2 Sets of 4 Repetitions",
+                R.drawable.curls, "Biceps Curls", "2 Sets of 12 Repetitions",
+                R.drawable.plank, "Plank", "35 seconds"
+            )
+        )
+        dayList.add(
+            Day("Day 7", "Leg Day",
+                R.drawable.squat, "Squats", "2 Sets of 15 Repetitions",
+                R.drawable.bulgarian_squat, "Bulgarian Squats", "2 Sets of 8 Repetitions",
+                R.drawable.crunches, "Crunches", "2 Sets of 10 Repetitions"
+            )
+        )
+        dayList.add(
+            Day("Day 8", "Rest Day",
                 R.drawable.diamond_pushup, "Diamond Push-Ups", "2 Sets of 6 Repetitions",
                 R.drawable.pike_pushup, "Pyke Push-Ups", "2 Sets of 6 Repetitions",
                 R.drawable.wide_pushup, "Wide Push-Ups", "2 Sets of 6 Repetitions"
             )
         )
         dayList.add(
-            Day("Day 6", "Push Day",
+            Day("Day 9", "Rest Day",
                 R.drawable.diamond_pushup, "Diamond Push-Ups", "2 Sets of 6 Repetitions",
                 R.drawable.pike_pushup, "Pyke Push-Ups", "2 Sets of 6 Repetitions",
                 R.drawable.wide_pushup, "Wide Push-Ups", "2 Sets of 6 Repetitions"
             )
         )
         dayList.add(
-            Day("Day 7", "Push Day",
+            Day("Day 10", "Push Day",
+                R.drawable.diamond_pushup, "Diamond Push-Ups", "2 Sets of 10 Repetitions",
+                R.drawable.pike_pushup, "Pyke Push-Ups", "2 Sets of 10 Repetitions",
+                R.drawable.wide_pushup, "Wide Push-Ups", "2 Sets of 10 Repetitions"
+            )
+        )
+        dayList.add(
+            Day("Day 11", "Pull Day",
+                R.drawable.pullup, "Pull-Ups", "2 Sets of 5 Repetitions",
+                R.drawable.curls, "Biceps Curls", "2 Sets of 12 Repetitions",
+                R.drawable.plank, "Plank", "40 seconds"
+            )
+        )
+        dayList.add(
+            Day("Day 12", "Leg Day",
+                R.drawable.squat, "Squats", "2 Sets of 15 Repetitions",
+                R.drawable.bulgarian_squat, "Bulgarian Squats", "2 Sets of 8 Repetitions",
+                R.drawable.crunches, "Crunches", "2 Sets of 10 Repetitions"
+            )
+        )
+        dayList.add(
+            Day("Day 13", "Rest Day",
+                R.drawable.diamond_pushup, "Diamond Push-Ups", "2 Sets of 10 Repetitions",
+                R.drawable.pike_pushup, "Pyke Push-Ups", "2 Sets of 10 Repetitions",
+                R.drawable.wide_pushup, "Wide Push-Ups", "2 Sets of 10 Repetitions"
+            )
+        )
+        dayList.add(
+            Day("Day 14", "Push Day",
+                R.drawable.diamond_pushup, "Diamond Push-Ups", "2 Sets of 10 Repetitions",
+                R.drawable.pike_pushup, "Pyke Push-Ups", "2 Sets of 10 Repetitions",
+                R.drawable.wide_pushup, "Wide Push-Ups", "2 Sets of 10 Repetitions"
+            )
+        )
+        dayList.add(
+            Day("Day 15", "Pull Day",
+                R.drawable.pullup, "Pull-Ups", "2 Sets of 5 Repetitions",
+                R.drawable.curls, "Biceps Curls", "2 Sets of 12 Repetitions",
+                R.drawable.plank, "Plank", "45 seconds"
+            )
+        )
+        dayList.add(
+            Day("Day 16", "Leg Day",
+                R.drawable.squat, "Squats", "2 Sets of 15 Repetitions",
+                R.drawable.bulgarian_squat, "Bulgarian Squats", "2 Sets of 8 Repetitions",
+                R.drawable.crunches, "Crunches", "2 Sets of 12 Repetitions"
+            )
+        )
+        dayList.add(
+            Day("Day 17", "Rest Day",
                 R.drawable.diamond_pushup, "Diamond Push-Ups", "2 Sets of 6 Repetitions",
                 R.drawable.pike_pushup, "Pyke Push-Ups", "2 Sets of 6 Repetitions",
                 R.drawable.wide_pushup, "Wide Push-Ups", "2 Sets of 6 Repetitions"
             )
         )
         dayList.add(
-            Day("Day 8", "Push Day",
+            Day("Day 18", "Push Day",
+                R.drawable.diamond_pushup, "Diamond Push-Ups", "3 Sets of 6 Repetitions",
+                R.drawable.pike_pushup, "Pyke Push-Ups", "3 Sets of 6 Repetitions",
+                R.drawable.wide_pushup, "Wide Push-Ups", "3 Sets of 6 Repetitions"
+            )
+        )
+        dayList.add(
+            Day("Day 19", "Pull Day",
+                R.drawable.pullup, "Pull-Ups", "2 Sets of 5 Repetitions",
+                R.drawable.curls, "Biceps Curls", "3 Sets of 12 Repetitions",
+                R.drawable.plank, "Plank", "50 seconds"
+            )
+        )
+        dayList.add(
+            Day("Day 20", "Leg Day",
+                R.drawable.squat, "Squats", "2 Sets of 20 Repetitions",
+                R.drawable.bulgarian_squat, "Bulgarian Squats", "3 Sets of 6 Repetitions",
+                R.drawable.crunches, "Crunches", "3 Sets of 8 Repetitions"
+            )
+        )
+        dayList.add(
+            Day("Day 21", "Rest Day",
                 R.drawable.diamond_pushup, "Diamond Push-Ups", "2 Sets of 6 Repetitions",
                 R.drawable.pike_pushup, "Pyke Push-Ups", "2 Sets of 6 Repetitions",
                 R.drawable.wide_pushup, "Wide Push-Ups", "2 Sets of 6 Repetitions"
+            )
+        )
+        dayList.add(
+            Day("Day 22", "Rest Day",
+                R.drawable.pullup, "Pull-Ups", "2 Sets of 4 Repetitions",
+                R.drawable.curls, "Biceps Curls", "2 Sets of 12 Repetitions",
+                R.drawable.plank, "Plank", "30 seconds"
+            )
+        )
+        dayList.add(
+            Day("Day 23", "Rest Day",
+                R.drawable.diamond_pushup, "Diamond Push-Ups", "2 Sets of 6 Repetitions",
+                R.drawable.pike_pushup, "Pyke Push-Ups", "2 Sets of 6 Repetitions",
+                R.drawable.wide_pushup, "Wide Push-Ups", "2 Sets of 6 Repetitions"
+            )
+        )
+        dayList.add(
+            Day("Day 24", "Push Day",
+                R.drawable.diamond_pushup, "Diamond Push-Ups", "3 Sets of 8 Repetitions",
+                R.drawable.pike_pushup, "Pyke Push-Ups", "3 Sets of 8 Repetitions",
+                R.drawable.wide_pushup, "Wide Push-Ups", "3 Sets of 8 Repetitions"
+            )
+        )
+        dayList.add(
+            Day("Day 25", "Pull Day",
+                R.drawable.pullup, "Pull-Ups", "3 Sets of 4 Repetitions",
+                R.drawable.curls, "Biceps Curls", "3 Sets of 12 Repetitions",
+                R.drawable.plank, "Plank", "55 seconds"
+            )
+        )
+        dayList.add(
+            Day("Day 26", "Leg Day",
+                R.drawable.squat, "Squats", "3 Sets of 15 Repetitions",
+                R.drawable.bulgarian_squat, "Bulgarian Squats", "3 Sets of 6 Repetitions",
+                R.drawable.crunches, "Crunches", "3 Sets of 12 Repetitions"
+            )
+        )
+        dayList.add(
+            Day("Day 27", "Rest Day",
+                R.drawable.squat, "Squats", "2 Sets of 12 Repetitions",
+                R.drawable.bulgarian_squat, "Bulgarian Squats", "2 Sets of 6 Repetitions",
+                R.drawable.crunches, "Crunches", "2 Sets of 8 Repetitions"
+            )
+        )
+        dayList.add(
+            Day("Day 28", "Push Day",
+                R.drawable.diamond_pushup, "Diamond Push-Ups", "3 Sets of 8 Repetitions",
+                R.drawable.pike_pushup, "Pyke Push-Ups", "3 Sets of 8 Repetitions",
+                R.drawable.wide_pushup, "Wide Push-Ups", "3 Sets of 8 Repetitions"
+            )
+        )
+        dayList.add(
+            Day("Day 29", "Pull Day",
+                R.drawable.pullup, "Pull-Ups", "3 Sets of 5 Repetitions",
+                R.drawable.curls, "Biceps Curls", "3 Sets of 12 Repetitions",
+                R.drawable.plank, "Plank", "60 seconds"
+            )
+        )
+        dayList.add(
+            Day("Day 30", "Leg Day",
+                R.drawable.squat, "Squats", "3 Sets of 15 Repetitions",
+                R.drawable.bulgarian_squat, "Bulgarian Squats", "3 Sets of 8 Repetitions",
+                R.drawable.crunches, "Crunches", "3 Sets of 12 Repetitions"
             )
         )
 
